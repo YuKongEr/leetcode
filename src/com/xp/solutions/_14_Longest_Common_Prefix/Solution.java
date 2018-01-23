ffbe152ee8b18430fd1352ffda8662815f75d57a
xp541130126 <541130126@qq.com>
2018-01-23 14:23:48 +0800

    Solved 14 Longest Common Prefix

---
+++ src/com/xp/solutions/_14_Longest_Common_Prefix/Solution.java
@@ -0,0 +1,38 @@
+package com.xp.solutions._14_Longest_Common_Prefix;
+
+/**
+ * @author yukong
+ * @version V1.0
+ * @Package com.xp.solutions._14_Longest_Common_Prefix
+ * @Description: TODO
+ * @date 2018/1/23 14:22
+ **/
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        return longestCommonPrefix(strs, 0, strs.length - 1);
+    }
+
+    private String longestCommonPrefix(String[] strs, int l, int r) {
+        if (l == r) {
+            return strs[l];
+        } else {
+            int mid = (l + r) / 2;
+            String lcpLeft = longestCommonPrefix(strs, l, mid);
+            String lcpRight = longestCommonPrefix(strs, mid + 1, r);
+            return commonPrefix(lcpLeft, lcpRight);
+        }
+    }
+
+    private String commonPrefix(String left, String right) {
+        int min = Math.min(left.length(), right.length());
+        for (int i = 0; i < min; i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                return left.substring(0, i);
+            }
+        }
+        return left.substring(0, min);
+    }
+}
