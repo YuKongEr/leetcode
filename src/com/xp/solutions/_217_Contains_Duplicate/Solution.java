ec17873f67885bda1d0af1a3ebe9ad013e1f9704
yukong <yukongcode@gmail.com>
2020-03-19 22:14:56 +0800

    Resloved Contains Duplicate

---
+++ src/com/xp/solutions/_217_Contains_Duplicate/Solution.java
@@ -0,0 +1,29 @@
+package com.xp.solutions._217_Contains_Duplicate;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个整数数组，判断是否存在重复元素。
+ * <p>
+ * 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3,1] 输出: true 示例 2:
+ * <p>
+ * 输入: [1,2,3,4] 输出: false 示例 3:
+ * <p>
+ * 输入: [1,1,1,3,3,4,3,2,4,2] 输出: true
+ *
+ * @author yukong
+ * @date 2020/3/19 22:14
+ */
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; ++i) {
+            if (nums[i] == nums[i + 1]) return true;
+        }
+        return false;
+    }
+}
