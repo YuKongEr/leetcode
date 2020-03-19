3694d49f2395fee69050ce281dd1711dd23d1b7a
yukong <yukongcode@gmail.com>
2020-03-19 20:27:39 +0800

    Resloved Jump Game

---
+++ src/com/xp/solutions/_45_Jump_Game_II/Solution.java
@@ -0,0 +1,8 @@
+package com.xp.solutions._45_Jump_Game_II;
+
+/**
+ * @author yukong
+ * @date 2020/3/19 09:45
+ */
+public class Solution {
+}
---
+++ src/com/xp/solutions/_55_Jump_Game/Solution.java
@@ -0,0 +1,8 @@
+package com.xp.solutions._55_Jump_Game;
+
+/**
+ * @author yukong
+ * @date 2020/3/19 08:15
+ */
+public class Solution {
+}
