104ada3be7dd320871a3c78f66ecfcd64aa80347
yukong <yukongcode@gmail.com>
2020-03-24 22:29:22 +0800

    Resovled Sliding Window Maximum

--- src/com/xp/solutions/_239_Sliding_Window_Maximum/Solution.java
+++ src/com/xp/solutions/_239_Sliding_Window_Maximum/Solution.java
@@ -1,8 +1,68 @@
 package com.xp.solutions._239_Sliding_Window_Maximum;
 
 /**
+ * 239. 滑动窗口最大值 给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+ * <p>
+ * 返回滑动窗口中的最大值。
+ * <p>
+ * <p>
+ * <p>
+ * 进阶：
+ * <p>
+ * 你能在线性时间复杂度内解决此题吗？
+ * <p>
+ * <p>
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3 输出: [3,3,5,5,6,7] 解释:
+ * <p>
+ * 滑动窗口的位置                最大值 ---------------               ----- [1  3  -1] -3  5  3  6  7       3 1 [3  -1  -3] 5  3 6
+ * 7       3 1  3 [-1  -3  5] 3  6  7       5 1  3  -1 [-3  5  3] 6  7       5 1  3  -1  -3 [5  3  6] 7       6
+ *
  * @author yukong
  * @date 2020/3/24 22:12
  */
 public class Solution {
+
+    /**
+     * 解法一 、动态规划
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        if (n * k == 0) {
+            return new int[0];
+        }
+        if (k == 1) {
+            return nums;
+        }
+        int[] left = new int[n];
+        int[] right = new int[n];
+        for (int i = 0; i < n; i++) {
+            // 从左到右分块 如果是块开头的元素
+            if (i % k == 0) {
+                left[i] = nums[i];
+            } else {
+                left[i] = Math.max(left[i - 1], nums[i]);
+            }
+            // 从右到左分块  如果是块开头的元素
+            int j = n - 1 - i;
+            if (j + 1 % k == 0) {
+                right[j] = nums[j];
+            } else {
+                right[j] = Math.max(right[j + 1], nums[j]);
+            }
+        }
+        int [] output = new int[n - k + 1];
+        for (int i = 0; i < n - k + 1; i++) {
+            output[i] = Math.max(left[i + k - 1], right[i]);
+        }
+
+        return output;
+
+    }
 }
