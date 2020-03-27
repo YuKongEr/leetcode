ba8827fbd76bf8a11c9fdabedc3a15cfdcd1d07e
yukong <yukongcode@gmail.com>
2020-03-27 23:28:01 +0800

    Resolved Excel sheet

---
+++ src/com/xp/solutions/_171_Excel_Sheet_Column_Number/Solution.java
@@ -0,0 +1,16 @@
+package com.xp.solutions._171_Excel_Sheet_Column_Number;
+
+/**
+ * @author yukong
+ * @date 2020/3/27 23:25
+ */
+public class Solution {
+    public int titleToNumber(String s) {
+        int ans = 0;
+        for(int i=0;i<s.length();i++) {
+            int num = s.charAt(i) - 'A' + 1;
+            ans = ans * 26 + num;
+        }
+        return ans;
+    }
+}
