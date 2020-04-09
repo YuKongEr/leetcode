7037ef23216c7781880084291ae925e21c913ce3
yukong <yukongcode@gmail.com>
2020-04-09 22:12:33 +0800

    Resolved Longest Increasing Subsequnce

---
+++ src/com/xp/solutions/_300_Longest_Increasing_Subsequence/Solution.java
@@ -0,0 +1,27 @@
+package com.xp.solutions._300_Longest_Increasing_Subsequence;
+
+/**
+ * @author yukongcode@gmail.com
+ * @date 2020/4/8 21:05
+ */
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < dp.length; i++) {
+            int maxval = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    maxval = Math.max(maxval, dp[j]);
+                }
+            }
+            dp[i] = maxval + 1;
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+}
