1d8aa188f9bda9a8593a7a6a057ad91e1b42cf00
yukong <yukongcode@gmail.com>
2020-04-27 10:02:21 +0800

    Resolved Search in Rotated Sorted Array

---
+++ src/com/xp/solutions/_33_Search_in_Rotated_Sorted_Array/Solution.java
@@ -0,0 +1,36 @@
+package com.xp.solutions._33_Search_in_Rotated_Sorted_Array;
+
+/**
+ * @author yukongcode@gmail.com
+ * @date 2020/4/27 10:01
+ */
+public class Solution {
+
+    // 二分排序
+    public int search(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1, mid = 0;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // 先根据 nums[mid] 与 nums[lo] 的关系判断 mid 是在左段还是右段
+            if (nums[mid] >= nums[lo]) {
+                // 再判断 target 是在 mid 的左边还是右边，从而调整左右边界 lo 和 hi
+                if (target >= nums[lo] && target < nums[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+        return -1;
+
+    }
+}
