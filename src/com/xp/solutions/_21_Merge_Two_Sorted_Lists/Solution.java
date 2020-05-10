a9d58cda60c56a9fb0fd82a4865347e0e82c5309
yukong <yukongcode@gmail.com>
2020-05-10 10:50:30 +0800

    Resolved 51 merge Two Sorted Lists

--- src/com/xp/solutions/_21_Merge_Two_Sorted_Lists/Solution.java
+++ src/com/xp/solutions/_21_Merge_Two_Sorted_Lists/Solution.java
@@ -67,5 +67,22 @@ public class Solution {
         }
         return p.next;
     }
+    // 递归
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+
+        if(l1 == null) {
+            return l2;
+        }
+        if(l2 == null) {
+            return l1;
+        }
+        if(l1.val <= l2.val){
+            l1.next = mergeTwoLists1(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists1(l1, l2.next);
+            return l2;
+        }
+    }
 
 }
