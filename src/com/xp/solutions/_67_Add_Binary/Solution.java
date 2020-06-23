c777dcdc550673fe772a1105bd45bffaa7c544a9
yukong <yukongcode@gmail.com>
2020-06-23 21:25:44 +0800

    Resolved Add Binary

---
+++ src/com/xp/solutions/_67_Add_Binary/Solution.java
@@ -0,0 +1,21 @@
+package com.xp.solutions._67_Add_Binary;
+
+/**
+ * @author yukong
+ * @date 2020/6/23 21:25
+ */
+public class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder ans = new StringBuilder();
+        int ca = 0;
+        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
+            int sum = ca;
+            sum += i >= 0 ? a.charAt(i) - '0' : 0;
+            sum += j >= 0 ? b.charAt(j) - '0' : 0;
+            ans.append(sum % 2);
+            ca = sum / 2;
+        }
+        ans.append(ca == 1 ? ca : "");
+        return ans.reverse().toString();
+    }
+}
