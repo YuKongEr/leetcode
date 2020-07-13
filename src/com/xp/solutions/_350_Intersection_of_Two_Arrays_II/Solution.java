90b725c3b611b852ec970ff7ebe19687d2b72252
yukong <yukongcode@gmail.com>
2020-07-13 20:04:00 +0800

    Resolved Intersection of Two Arrays II

---
+++ src/com/xp/solutions/_350_Intersection_of_Two_Arrays_II/Solution.java
@@ -0,0 +1,40 @@
+package com.xp.solutions._350_Intersection_of_Two_Arrays_II;
+
+import java.util.Arrays;
+
+/**
+ * 350. 两个数组的交集 II
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2,2]
+ * 示例 2:
+ * <p>
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[4,9]
+ *
+ * @author yukong
+ * @date 2020/7/13 20:03
+ */
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0, k = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums2[j] < nums1[i]) {
+                j++;
+            } else {
+                nums1[k++] = nums1[i++];
+                j++;
+            }
+        }
+        return Arrays.copyOfRange(nums1, 0, k);
+    }
+}
