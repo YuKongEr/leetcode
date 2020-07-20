5afca52e3bd6082f80af1866a6d888e68cadb8a7
yukong <yukongcode@gmail.com>
2020-07-20 17:40:35 +0800

    Resolved 167 Two Sum II

--- src/com/xp/solutions/_167_Two_Sum_II/Solution.java
+++ src/com/xp/solutions/_167_Two_Sum_II/Solution.java
@@ -24,4 +24,19 @@ public class Solution {
         return result;
     }
 
+    public int[] twoSum1(int[] numbers, int target) {
+        int low = 0, high = numbers.length - 1;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target) {
+                return new int[]{low + 1, high + 1};
+            } else if (sum < target) {
+                ++low;
+            } else {
+                --high;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
 }
