f45fdc7abf7b8b90460b87498ce5a3b925556628
yukong <yukongcode@gmail.com>
2020-07-22 20:38:04 +0800

    Resolved Find minimum in rotated sort array

---
+++ src/com/xp/solutions/practice/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,35 @@
+package com.xp.solutions.practice;
+
+/**
+ * 剑指 Offer 11. 旋转数组的最小数字
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：[3,4,5,1,2]
+ * 输出：1
+ * 示例 2：
+ * <p>
+ * 输入：[2,2,2,0,1]
+ * 输出：0
+ *
+ * @author yukong
+ * @date 2020/7/22 20:36
+ */
+public class FindMinimumInRotatedSortedArray {
+    public int minArray(int[] numbers) {
+        int low = 0;
+        int high = numbers.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (numbers[pivot] < numbers[high]) {
+                high = pivot;
+            } else if (numbers[pivot] > numbers[high]) {
+                low = pivot + 1;
+            } else {
+                high -= 1;
+            }
+        }
+        return numbers[low];
+    }
+}
