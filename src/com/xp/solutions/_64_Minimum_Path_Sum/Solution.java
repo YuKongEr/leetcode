89ab25bde89d8bfc2c5380291c6c1eff034eff08
yukong <yukongcode@gmail.com>
2020-07-23 21:32:27 +0800

    Resolved Minumum Path Sum

--- src/com/xp/solutions/_64_Minimum_Path_Sum/Solution.java
+++ src/com/xp/solutions/_64_Minimum_Path_Sum/Solution.java
@@ -45,6 +45,28 @@ public class Solution {
 
     }
 
+    public int minPathSum1(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] dp = new int[n + 1][m + 1];
+        dp[0][0] = grid[0][0];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+                }
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+
 
     public static void main(String[] args) {
         int[][] arr ={{1,2,5},{3,2,1}};
