4e3ed9ceb437ae0839b90bd50e29cf135b1ddda4
yukong <yukongcode@gmail.com>
2020-08-02 10:10:48 +0800

    Resolved 26 Remove Duplicate from Sorted Array

---
+++ src/com/xp/solutions/_26_Remove_Duplicates_from_Sorted_Array/Solution.java
@@ -0,0 +1,44 @@
+package com.xp.solutions._26_Remove_Duplicates_from_Sorted_Array;
+
+/**
+ * 26. 删除排序数组中的重复项
+ * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定数组 nums = [1,1,2],
+ * <p>
+ * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+ * <p>
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 示例 2:
+ * <p>
+ * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+ * <p>
+ * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+ * <p>
+ * 你不需要考虑数组中超出新长度后面的元素。
+ *
+ * @author yukong
+ * @date 2020/8/2 10:08
+ */
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int curr = 0, next = 0;
+        while(next < nums.length) {
+            if (nums[curr] != nums[next]) {
+                curr++;
+                nums[curr] = nums[next];
+            }
+            next++;
+        }
+        return ++curr;
+    }
+}
