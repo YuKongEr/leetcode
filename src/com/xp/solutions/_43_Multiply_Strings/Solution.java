38e2d922ef4104fd746c6a1535e2e8f3b47ea383
yukong <yukongcode@gmail.com>
2020-08-13 20:58:11 +0800

    Resolved 43 Multipy Strings

---
+++ src/com/xp/solutions/_43_Multiply_Strings/Solution.java
@@ -0,0 +1,38 @@
+package com.xp.solutions._43_Multiply_Strings;
+
+/**
+ * @author yukong
+ * @date 2020/8/13 20:52
+ */
+public class Solution {
+    public String multiply(String num1, String num2) {
+        int n1 = num1.length()-1;
+        int n2 = num2.length()-1;
+        if(n1 < 0 || n2 < 0) {
+            return "";
+        }
+        int[] mul = new int[n1+n2+2];
+
+        for(int i = n1; i >= 0; --i) {
+            for(int j = n2; j >= 0; --j) {
+                int bitmul = (num1.charAt(i)-'0') * (num2.charAt(j)-'0');
+                // 先加低位判断是否有新的进位
+                bitmul += mul[i+j+1];
+
+                mul[i+j] += bitmul / 10;
+                mul[i+j+1] = bitmul % 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        // 去掉前导0
+        while(i < mul.length-1 && mul[i] == 0) {
+            i++;
+        }
+        for(; i < mul.length; ++i) {
+            sb.append(mul[i]);
+        }
+        return sb.toString();
+    }
+}
