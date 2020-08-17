ab073a42b43aa6178b9a54ca3f09ca3e8c60e87c
yukong <yukongcode@gmail.com>
2020-08-17 09:18:26 +0800

    Resolved 110 Balanced Binary Tree

---
+++ src/com/xp/solutions/_110_Balanced_Binary_Tree/Solution.java
@@ -0,0 +1,54 @@
+package com.xp.solutions._110_Balanced_Binary_Tree;
+
+import com.xp.solutions.TreeNode;
+
+/**
+ * 110. 平衡二叉树
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * <p>
+ * 本题中，一棵高度平衡二叉树定义为：
+ * <p>
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定二叉树 [3,9,20,null,null,15,7]
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回 true 。
+ * <p>
+ * 示例 2:
+ * <p>
+ * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \
+ * 3   3
+ * / \
+ * 4   4
+ * 返回 false 。
+ *
+ * @author yukong
+ * @date 2020/8/17 09:17
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+    }
+}
