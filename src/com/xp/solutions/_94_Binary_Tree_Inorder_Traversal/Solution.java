2265edfa8b86bda1a4cf94f062c8dce84f6be8fa
yukong <yukongcode@gmail.com>
2020-09-14 09:30:53 +0800

    Resolved 94 Binary Tress Traversal

---
+++ src/com/xp/solutions/_94_Binary_Tree_Inorder_Traversal/Solution.java
@@ -0,0 +1,46 @@
+package com.xp.solutions._94_Binary_Tree_Inorder_Traversal;
+
+import com.xp.solutions.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 94. 二叉树的中序遍历
+ * 给定一个二叉树，返回它的中序 遍历。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [1,null,2,3]
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * 输出: [1,3,2]
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ * @author yukong
+ * @date 2020/9/14 09:29
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            TreeNode pre = stack.pop();
+            res.add(pre.val);
+            root = pre.right;
+        }
+        return res;
+    }
+}
